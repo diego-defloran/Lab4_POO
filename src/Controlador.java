6baa723163b8c2618490facdc4d52fce3a2b1a26
diego-defloran <defl21565@uvg.edu.gt>
2021-11-14 09:30:10 -0600

    Initial commit

---
+++ src/Controlador.java
@@ -0,0 +1,14 @@
+/*
+ * @author Diego De Florán
+ * @author Javier Rucal
+ */
+public class Controlador {
+	/*
+	 * @args String[] args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
